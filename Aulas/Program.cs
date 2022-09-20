fa9ebda2a34b884c699334ba1af2254db456c44e
Pedro Henrique Martins <p89martins@gmail.com>
2022-09-20 09:22:52 -0300

    Listas, Arrays e Foreach

---
+++ Aulas/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+  
+        List<string> listaString = new List<string>(); //Sintaxe de uma Lista
+
+        listaString.Add("MG");
+        listaString.Add("BA");
+        listaString.Add("SP");
+        listaString.Add("RJ");
+
+
+        Console.WriteLine("Percorrendo o Array com o FOR:");
+        for(int contador = 0; contador < listaString.Count; contador++)
+        {
+            Console.WriteLine($"Posicao N: {contador} - {listaString[contador]}");
+        }
+
+        Console.WriteLine("Percorrendo o Array com o FOREACH:");
+        int contadorForeach = 0;
+        foreach(string item in listaString)
+        {
+            Console.WriteLine($"Posicao N: {contadorForeach} - {item}");    
+            contadorForeach++;
+        }
+  
+
+        Console.WriteLine($"Itens na minha lista: {listaString.Count} = Capacidade: {listaString.Capacity}");
+
+        listaString.Add("SC");
+  
+        Console.WriteLine($"Itens na minha lista: {listaString.Count} = Capacidade: {listaString.Capacity}");
+
+        listaString.Add("ES");
+        
+  
+  
+  
+  
+  
+        // int[] arrayInteiros = new int[4];
+
+        // arrayInteiros[0] = 73;
+        // arrayInteiros[1] = 64;
+        // arrayInteiros[2] = 50;
+        // arrayInteiros[3] = 73;
+
+        
+        // Console.WriteLine("Percorrendo o Array com o FOR:");
+        // for (int contador = 0; contador < arrayInteiros.Length; contador++)
+        // {
+        //     Console.WriteLine($"Posicao N {contador} - {arrayInteiros[contador]}");
+        // }
+
+
+        // //Copiando valores de um array para um outro com capacidade maior
+
+        // int[] arraysInteirosDobrados = new int[arrayInteiros.Length * 2];
+        // Array.Copy(arrayInteiros, arraysInteirosDobrados, arrayInteiros.Length);
+
+
+
+
+        // //Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2); //Como dobrar(redimensionar) a capacidade de um Array
+
+        // Console.WriteLine("Percorrendo o Array com o FOREACH:");
+
+
+
+
+
+        // int contadorForeach = 0;
+        // foreach(int valor in arrayInteiros) //Para cada valor do ArrayInteiros joga para a variável Valor
+        // {
+        //     Console.WriteLine($"Posicao N {contadorForeach} - {valor}");
+        //     contadorForeach++;
+        // }
+    }
+}
--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -1,72 +1,14 @@
-﻿using System;
-using ExemplosFundamentos.Common.Models;
+﻿using ExemplosFundamentos.Models;
 
 
-int quantidadeEmEstoque =10;
-int quantidadeDeCompra = 4;
-bool possivelVenda = quantidadeEmEstoque >= quantidadeDeCompra;
+int arryInteiros = new int [3];
 
+arryInteiros[0] = 73;
+arryInteiros[1] = 64;
+arryInteiros[2] = 50;
+//arryInteiros[3] = 73;
 
-Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
-Console.WriteLine($"Quantidade para compra: {quantidadeDeCompra}");
-Console.WriteLine($"É possivel realizar a venda? {possivelVenda}");
-
-
-
-if(possivelVenda)
-{
-    Console.WriteLine("Venda realizada.");
-}
-else
-{
-    Console.Write("Desculpe. Não temos a quantidado em estoque.");
-}
-
-
-/*
-// Partes de um objeto 
-Pessoa pessoa1 = new Pessoa();
-
-pessoa1.Nome =  "Pedro";
-pessoa1.Idade = 33;
-pessoa1.Apresentar();
-
-
-
-// Trabalhando com datas
-DateTime dataAtual =  DateTime.Now.AddDays(5);
-Console.WriteLine(dataAtual.ToString("dd/MM/yyyy"));
-
-
-
-//Conversao de variaveis(Cast - Casting)
-
-int a = Convert.ToInt32("5");
-int b = int.Parse("5"); //O parse nao converte NULL
-int c = Convert.ToInt32(null); //Convert null para ZERO, mais aceitavel
-
-Console.WriteLine(a);
-Console.WriteLine(b);
-Console.WriteLine(c);
-
-
-string g = "15-";
-int h = 0;
-
-int.TryParse(g, out b); //Forma segura tente esse valor invalido se nao devolva H
-
-//Conversap de variaveis String
-
-int inteiro = 5;
-string d = inteiro.ToString();
-
-Console.WriteLine(d);
-
-
-//Casting implicito
-
-int e = 5;
-double f = e;
-Console.WriteLine(b); //Pois um iNT cabe dentro de um DOUBLE,ao contrario teria q usar o convert.
-
-*/
+for(int contador = 0; contador < arryInteiros.Length; contador++)
+    {
+        Console.WriteLine($"Posicao N {contador} - {arrayInteiros[contador]}");
+    }
